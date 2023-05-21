11e3eac9ce2807260eeb4d27860850845220e660
Alex Chernov <nak4442@gmail.com>
2023-05-22 01:45:39 +0300

    Поездки + отсортированные фото

--- script.py
+++ script.py
@@ -1,8 +1,13 @@
-from telebot import TeleBot
+from telebot import TeleBot, types
 from config import TOKEN
 from keyboards import keyboard
 
 
+COMMANDS = [
+    "🚶‍♂️ Пеший",
+    "⛵️ Водный",
+    "🚴‍♂️ Велосипедный"
+]
 bot = TeleBot(TOKEN)
 
 @bot.message_handler(commands=["start"])
@@ -14,5 +19,25 @@ def start(message):
         reply_markup=keyboard("🚶‍♂️ Пеший", "⛵️ Водный", "🚴‍♂️ Велосипедный")
     )
 
+@bot.message_handler(content_types=["text"])
+def any(message : types.Message):
+    if message.text not in COMMANDS:
+        bot.send_message(
+            message.chat.id,
+            text="Я тебя не понимаю, брат🤣"
+        )
+    else:
+        if message.text == COMMANDS[0]:
+            bot.send_message(
+                message.chat.id,
+                text="Выберите регион похода",
+                reply_markup=keyboard("Ивановский", "Шуйский", "Тейковский", "Юрьевецкий", "Плесский", "Палехский", "Вичужский")
+            )
+        elif message.text == COMMANDS[1]:
+            bot.send_message(
+                message.chat.id,
+                text="Выберите реку",
+                reply_markup=keyboard("Уводь", "Теза", "Волга", "Клязьма", "Шуя", "Нерль", "Лух")
+            )
 
 bot.infinity_polling()
